bcd76474a0f1c493a5e34145ab645d85962bfa4f
darrentanyf <89795922+darrentanyf@users.noreply.github.com>
2021-11-24 17:10:49 +0800

    Upload Again

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors")
 const pool = require("./db")
+const path = require("path")
 
 const usersController = require("./controllers/users")
 const guidesController = require("./controllers/guides")
