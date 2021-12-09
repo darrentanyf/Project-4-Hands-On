bc01ee8ecfa3203da48406e239ababead11c4f51
darrentanyf <89795922+darrentanyf@users.noreply.github.com>
2021-12-09 23:47:40 +0800

    Installed and Used React Toastify for Login Page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import axios from "axios";
 import { useState } from "react"
 import Navibar from "./Navibar"
+import Categories from "../components/Categories"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Login() {
     const [Auth, setAuth] = useState(0)
+
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log("USERNAME", event.currentTarget.username.value);
@@ -19,19 +23,40 @@ export default function Login() {
             .then((response) => {
                 console.log(response.data.authenticated)
                 console.log(response.data.message)
-                // console.log(response.data.token)
 
                 setAuth(response.data.authenticated)
                 if (response.data.authenticated) {
+                    toast.info(response.data.message, {
+                        position: "top-center",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        });
+
                     window.localStorage.setItem("token", response.data.token);
                     setTimeout(function () {
                         window.location.replace("/");
                     }, 1300);
                 }
+                else {
+                    toast.error(response.data.message, {
+                        position: "top-center",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        });
+                }
             });
     };
 
     return (
+        <div>
         <div style={{"background-color": "#F1F1F1"}}>
             <Navibar />
             <div class="container" style={{ padding: "60px" }}>
@@ -60,23 +85,23 @@ export default function Login() {
                                     />
                                 </div>
                             </div>
-                            <button type="submit" id="loginButton">
+                            <button type="submit" id="loginSignupButton">
                                 Submit
                             </button>
                             <br></br>
                         </form>
-                        {Auth === 0 ? (<br></br>)
+                        {/* {Auth === 0 ? (<br></br>)
                             : Auth === true ? (
-                                <div class="alert alert-success" role="alert">
+                                <div class="alert alert-success" role="alert" style={{ "border-radius": 0, margin: 10}}>
                                     Login is successful!
                                 </div>
                             )
                                 : (
-                                    <div class="alert alert-danger" role="alert">
+                                    <div class="alert alert-danger" role="alert" style={{ "border-radius": 0, margin: 10}}>
                                         Username or Password is invalid.
                                     </div>
                                 )
-                        }
+                        } */}
                         <div class="row" style={{"border-top": "1px solid #EFF1F2", "border-bottom": "1px solid #EFF1F2", margin:10, padding: 25, display: "flex", "justifyContent": "center"}}>
                             New to Hands On? <a href="/signup" style={{width: 80, height:20, textDecoration: "none"}}>Sign Up</a>
                         </div>
@@ -87,6 +112,9 @@ export default function Login() {
                     <div class="col"></div>
                 </div>
             </div>
+        </div>
+        <Categories />
+        < ToastContainer theme="colored"/>
         </div>
     );
 }
