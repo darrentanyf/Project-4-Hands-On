d97231280fed02220c3fd54a314347f8ec9057fe
darrentanyf <89795922+darrentanyf@users.noreply.github.com>
2021-12-09 22:53:46 +0800

    Equal Spacing

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -6,14 +6,15 @@ const Categories = () => {
 
     
     return (
-        <div class="container" style={{display:"flex", "border-top": "1px solid #EFF1F2", "border-bottom": "1px solid #EFF1F2"}}>
-            <div class="row w-100" style={{display:"flex", "flex-wrap": "nowrap", "justifyContent": "center", margin: "0 auto", padding: 20}}>
+        <div class="container" style={{ "border-top": "1px solid #EFF1F2", "border-bottom": "1px solid #EFF1F2", padding: 20}}>
+            <div class="row w-100" style={{display:"inline", margin: "0 auto", padding: 20}}>
             <a href="/signup" id="categoriesLink" >Arts</a>
             <a href="/signup" id="categoriesLink" >Daily Necessities</a>
             <a href="/signup" id="categoriesLink" >Kitchen Ideas</a>
             <a href="/signup" id="categoriesLink" >Entertainment</a>
             <a href="/signup" id="categoriesLink" >Home Decoration</a>
             <a href="/signup" id="categoriesLink" >Life Hacks</a>
+            <a href="/signup" id="categoriesLink" >Easy Fix</a>
             </div>
 
         </div>
