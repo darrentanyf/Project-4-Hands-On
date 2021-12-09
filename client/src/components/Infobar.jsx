85880854cd5f400300792d0e2232964de5b2ec2a
darrentanyf <89795922+darrentanyf@users.noreply.github.com>
2021-12-09 23:16:37 +0800

    Updated Styling of InfoBar

--- client/src/components/Infobar.jsx
+++ client/src/components/Infobar.jsx
@@ -22,11 +22,20 @@ const Infobar = () => {
 
 
     return (
-        <Container>
-            <Row style={{ border: "1px solid ", padding: 20, margin: 25, "box-shadow": "1px 2px lightgrey", "border-radius": 5 }}>
-                <Col><b>{guidesCount}</b><br></br><b>Guides Created</b></Col>
-                <Col><b>{stepsCount}</b><br></br><b>Steps</b></Col>
-                <Col><b>{successCount}</b><br></br><b>Success</b></Col>
+        <Container style={{margin: 20}}>
+            <Row style={{ border: "1px solid #EFF1F2", "box-shadow": "rgba(149, 157, 165, 0.2) 0px 8px 24px" }}>
+                <Col style={{padding: 20}}>
+                    <p style={{"font-size": 30, "font-weight": "bold", margin: 0, color: "#2C79AD" }}>{guidesCount}</p>
+                    guides created
+                </Col>
+                <Col style={{padding: 20}}>
+                    <p style={{"font-size": 30, "font-weight": "bold", margin: 0, color: "#2C79AD" }}>{stepsCount}</p>
+                     steps followed
+                </Col>
+                <Col style={{padding: 20}}>
+                <p style={{"font-size": 30, "font-weight": "bold", margin: 0, color: "#2C79AD" }}>{successCount}</p>
+                    sucess stories
+                </Col>
             </Row>
         </Container>
     )
