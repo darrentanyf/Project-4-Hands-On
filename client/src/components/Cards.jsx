4b845ebb3c4e9bd4e5096e5f2059a717de040adf
darrentanyf <89795922+darrentanyf@users.noreply.github.com>
2021-12-10 02:05:58 +0800

    Updated Homepage Styling

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -4,7 +4,6 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
 
 const Cards = () => {
     const [guides, setGuides] = useState([])
@@ -25,24 +24,33 @@ const Cards = () => {
 
     return (
         <div>
-            <Row >
-                { guides?.map((guide,index)=>(
-                        <Col style={{ display: "flex", justifyContent: "center" }}>
-                        <Card bg="light" text="dark"  style={{ width: "19rem", margin: 10 }}>
+            <div class="row" style={{ dispaly: "flex" }}>
+                <div class="col" style={{ display: "flex", justifyContent: "center", "flex-wrap": "wrap" }}>
+                    {guides?.map((guide, index) => (
+                        <Card bg="light" text="dark" style={{ width: 338, "margin": 20 }}>
                             <Card.Img variant="top" src={guides[index]?.guides_img} height="180px" />
                             <Card.Body>
                                 <Card.Title>{guides[index]?.name}</Card.Title>
                                 <Card.Text>
-                                    {guides[index]?.description.slice(0,99)+"..."}
+                                    {guides[index]?.description.slice(0, 99) + "..."}
                                 </Card.Text>
                                 <a href={`/guides/${guides[index]?.guides_id}`} id={`cardButton${index}`}><Button variant="outline-dark" >Check it out</Button></a>
                             </Card.Body>
                         </Card>
-                    </Col>
-                ))}
-            </Row>
-                <br></br>
-                <p style={{"border-top": "1px solid lightgrey", margin: 15}}>HandsOn Pte Ltd</p>
+                    ))}
+                </div>
+                <nav aria-label="Page navigation example">
+                    {/* <ul class="pagination">
+                        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                    </ul> */}
+                </nav>
+            </div>
+            <br></br>
+            <p style={{ "border-top": "1px solid lightgrey", margin: 15 }}>HandsOn Pte Ltd</p>
         </div>
     );
 };
