35a3e9354526c00f1d7ff334aec57042cd157e1f
darrentanyf <89795922+darrentanyf@users.noreply.github.com>
2021-12-09 22:12:08 +0800

    Sign Up Layout Redesign Done

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -9,27 +9,18 @@ import Navibar from "./Navibar"
 export default function Signup() {
     const [Auth, setAuth] = useState(0)
     const [Message, setMessage] = useState(null)
-    // const [Preview, setPreview] = useState(null)
 
-    // const handlePreview = (event) =>{
-    //     console.log("LINK PREVIEW",event.currentTarget.value)
-    //     setPreview(URL.createObjectURL(event.currentTarget.value))
-    // }
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        // console.log("USERNAME", event.currentTarget.username.value);
-        // console.log("EMAIL", event.currentTarget.email.value);
-        // console.log("Password", event.currentTarget.password.value);
-        // console.log("profileimg", event.currentTarget.profileimg.value);
-        // const data = new FormData(event.currentTarget);
+
 
         axios
             .post("/api/users/signup", {
                 username: event.currentTarget.username.value,
                 email: event.currentTarget.email.value,
                 password: event.currentTarget.password.value,
-                // profileimg: event.currentTarget.profileimg.value,
+
             })
             .then((response) => {
 
@@ -55,39 +46,44 @@ export default function Signup() {
     }
 
     return (
-        <div>
+        <div style={{ "background-color": "#F1F1F1" }}>
             <Navibar />
-            <h2>Sign Up</h2>
-            <div class="container col-md-4">
-                <form onSubmit={handleSubmit}>
-                    <div class="form-group">
-                        <label for="username">Username</label>
-                        <input
-                            type="username"
-                            class="form-control"
-                            id="username"
-                            placeholder="Enter Username"
-                        />
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input
-                            type="email"
-                            class="form-control"
-                            id="email"
-                            placeholder="Enter Email"
-                        />
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input
-                            type="password"
-                            class="form-control "
-                            id="password"
-                            placeholder="Enter Password"
-                        />
-                    </div>
-                    {/* <div class="form-group">
+            <div class="container" style={{ padding: "60px" }}>
+                <div class="row" >
+                    <div class="col"></div>
+                    <div class="col-6" style={{ border: "1px solid #EFF1F2", width: 450, "backgroundColor": "white" }}>
+                    <div class="row" style={{ margin: 25 }}><h2>Sign Up</h2></div>
+                        <form onSubmit={handleSubmit}>
+                        <div class="row" style={{ margin: 15 }}>
+                            <div class="form-group" style={{ "margin-bottom": 20 }}>
+                                <input
+                                    type="username"
+                                    class="form-control"
+                                    id="username"
+                                    placeholder="Username"
+                                    style={{ "border-radius": 0, height: 47 }}
+                                />
+                            </div>
+                            <div class="form-group" style={{ "margin-bottom": 20 }}>
+                                <input
+                                    type="email"
+                                    class="form-control"
+                                    id="email"
+                                    placeholder="Email"
+                                    style={{ "border-radius": 0, height: 47 }}
+                                />
+                            </div>
+                            <div class="form-group" style={{ "margin-bottom": 10 }}>
+                                <input
+                                    type="password"
+                                    class="form-control "
+                                    id="password"
+                                    placeholder="Password"
+                                    style={{ "border-radius": 0, height: 47 }}
+                                />
+                            </div>
+                            </div>
+                            {/* <div class="form-group">
                         <label for="profileimg">Profile Image</label>
                         <input
                             type="text"
@@ -96,7 +92,7 @@ export default function Signup() {
                             placeholder="Upload Profile Image"
                         />
                     </div> */}
-                    {/* <div class="input-group mb-3">
+                            {/* <div class="input-group mb-3">
                         <div class="input-group-prepend">
                             <span class="input-group-text">Upload</span>
                         </div>
@@ -106,24 +102,32 @@ export default function Signup() {
                         </div>
                     </div>
                     <img src={Preview} alt="..." class="img-thumbnail"/> */}
-                    <button type="submit" class="btn btn-primary">
-                        Submit
-                    </button>
-                    <br></br>
-                </form>
-                {Auth === 0 ? (<br></br>)
-                    : Auth === true ? (
-                        <div class="alert alert-success" role="alert">
-                            Sign Up is successful!
-                        </div>
-                    )
-                        : (
-                            <div class="alert alert-danger" role="alert">
-                                {Message}
+                            <div>
+                            <button type="submit" id="loginSignupButton">
+                                Create account
+                            </button>
                             </div>
-                        )
-                }
+                            <br></br>
+                        </form>
+                            <div class="row" style={{width: "88%", margin: "0 auto"}}>
+                            <p style={{"font-size": 13}}>By signing up, you agree to our Privacy Policy, Cookie Policy and Terms of Use. </p>
+                        </div>
+                        {Auth === 0 ? (<br></br>)
+                            : Auth === true ? (
+                                <div class="alert alert-success" role="alert" style={{ "border-radius": 0}}>
+                                    Sign Up is successful!
+                                </div>
+                            )
+                                : (
+                                    <div class="alert alert-danger" role="alert" style={{ "border-radius": 0}}>
+                                        {Message}
+                                    </div>
+                                )
+                        }
+                    </div>
+                    <div class="col"></div>
+                    </div>
+                </div>
             </div>
-        </div>
-    )
+            )
 }
